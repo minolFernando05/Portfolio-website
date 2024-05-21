e3e1eee5b6bbf8cb5ed2be0e4b439b45b785c510
minolFernando05 <149147985+minolFernando05@users.noreply.github.com>
2024-05-21 23:51:26 +0530

    Update NavBar component and add mobile menu functionality

--- portfolio-website-project/src/components/NavBar.jsx
+++ portfolio-website-project/src/components/NavBar.jsx
@@ -1,15 +1,44 @@
 import React,{useState} from 'react';
 import './NavBar.css';
 import navLogo from "../assets/ryan-web-logo.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faXmark,faBars} from '@fortawesome/free-solid-svg-icons'
+
+
+
 
 // import Img1 from "./assets/NavBarLogo.png";
 
 function NavBar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenue = () => setClick(false);
     return (
         <>
         <nav className="navbar">
             <div className="navbar-container">
-                <img src={navLogo} alt="navigation-bar-logo" />
+                <img className='navbarlogo' src={navLogo} alt="navigation-bar-logo" />
+                <div className="menueIcon" onClick={handleClick}>
+                    <FontAwesomeIcon icon={click ? faBars : faXmark}/>
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <a href="/" className='nav-links' onClick={closeMobileMenue}>
+                            About Me
+                        </a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/about" className='nav-links' onClick={closeMobileMenue}>
+                            Projects
+                        </a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/services" className='nav-links' onClick={closeMobileMenue}>
+                            Skills
+                        </a>    
+                    </li>
+                </ul>
             </div>
         </nav>
         </>
@@ -18,3 +47,9 @@ function NavBar() {
 }
 
 export default NavBar;
+
+{/* <FontAwesomeIcon icon={faXmark} rotation={90} beat /> */}
+                {/* const element = <FontAwesomeIcon icon={faHippo}/>
+                const element = <FontAwesomeIcon icon={faHouse} rotation={90} beat />
+                <FontAwesomeIcon icon={faBars} />
+                <FontAwesomeIcon icon="fa-light fa-house" /> */}
