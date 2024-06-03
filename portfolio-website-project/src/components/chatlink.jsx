d77bfb73319bcf4388919db885cef1dae81e5803
minolFernando05 <149147985+minolFernando05@users.noreply.github.com>
2024-06-04 01:46:02 +0530

    Update CSS for ProjectNav and Datapel pages

---
+++ portfolio-website-project/src/components/chatlink.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './chatlink.css';
+
+
+
+function chatLink() {
+    return (
+        <div className='chatLink'>
+            <div className='main'>
+                Interested in my work?
+            </div>
+            <div className='sub'>
+                <Link to="/food">
+                Let's chat &#8594;
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default chatLink;
